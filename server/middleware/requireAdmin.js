01d40eadde979c4441b0b2e872f8ccef2d4744e3
RaiTo_oNe <monhzululaanhuu9@gmail.com>
2026-02-02 10:29:17 +0800

    Delete server directory

--- server/middleware/requireAdmin.js
+++
@@ -1,6 +0,0 @@
-module.exports = function requireAdmin(req, res, next) {
-  if (!req.session || !req.session.user || req.session.user.role !== 'admin') {
-    return res.status(403).json({ ok: false, error: 'Admin access required' });
-  }
-  next();
-}
--- server/middleware/requireResearcher.js
+++
@@ -1,6 +0,0 @@
-module.exports = function requireResearcher(req, res, next) {
-  if (!req.session || !req.session.user || (req.session.user.role !== 'researcher' && req.session.user.role !== 'admin')) {
-    return res.status(403).json({ ok: false, error: 'Researcher or admin access required' });
-  }
-  next();
-}
